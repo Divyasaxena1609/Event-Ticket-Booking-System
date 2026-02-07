a728d50691f62a8a4335ddb6835d7a76b84022bf
Divyasaxena1609 <saxenadivyaee@gmail.com>
2026-02-07 22:00:16 +0530

    remove common framework and fix parent pom

--- auth-service/src/main/java/com/ticketbooking/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/ticketbooking/auth_service/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.ticketbooking.auth;
+package com.ticketbooking.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- common-framework/src/main/java/com/ticketbooking/common/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.ticketbooking.common.exception;
-
-public class ResourceNotFoundException extends BusinessException {
-
-    public ResourceNotFoundException(String message) {
-        super("RESOURCE_NOT_FOUND", message);
-    }
-}
-
